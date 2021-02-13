1698d3d0f54ff0716502716ed9c6515972f43bdd
Matthew Lyles <matthewlyles@Matthews-MacBook-Pro.local>
2021-02-13 13:52:10 -0800

    Setting up the project

---
+++ TikTok/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  TikTok
+//
+//  Created by Matthew Lyles on 2/13/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+    }
+
+}
+
