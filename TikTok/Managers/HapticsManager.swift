c750feed403285aabc957a170d4ee430aeef4620
Matthew Lyles <matthewlyles@Matthews-MacBook-Pro.local>
2021-02-20 22:47:26 -0800

    added haptic manager class

---
+++ TikTok/Managers/HapticsManager.swift
@@ -0,0 +1,27 @@
+//
+//  HapticsManager.swift
+//  TikTok
+//
+//  Created by Matthew Lyles on 2/20/21.
+//
+
+import Foundation
+import UIKit
+
+final class HapticsManager {
+    
+    public static let shared = HapticsManager()
+    private init() {}
+    
+    // Public Methods
+    public func vibrateForSelection(for type: UINotificationFeedbackGenerator.FeedbackType) {
+        DispatchQueue.main.async {
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(type)
+        }
+    }
+    
+    
+}
+
